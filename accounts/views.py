6e3f3c2f955f9750af71c5463497b983ca34b6b2
zafarkaharov <zkaharov@gmail.com>
2019-08-08 11:06:45 -0700

    created register view

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
+from django.contrib import auth
+from django.contrib.auth.models import User
 
 # Create your views here.
+
+def register(request):
+    if request.method == 'POST':
+        first_name = request.POST['first_name']
+        last_name = request.POST['last_name']
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(username=username).exists():
+                return render(request, 'register.html', {'eror': 'Username is already taken'})
+            else:
+                if User.objects.filter(email=email).exists():
+                    return render(request, 'register.html', {'error': 'Email already exists. Please Log In.'})
+                else:
+                    user = User.objects.create_user(
+                        username=username, password=password,
+                        email=email, first_name=first_name,
+                        last_name=last_name
+                    )
+                    user.save()
+                    return redirect('login')
+        else:
+            return render(request, 'register.html', {'error': 'Password does not match'})
+else:
+    return render(request, 'register.html',)       
+
+
+
