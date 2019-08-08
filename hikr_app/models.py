418e25f3f9ab7423a62d3bb1a9d6678291173066
Scottlove <haleyscottlove@gmail.com>
2019-08-08 09:57:52 -0700

    added POST model

--- hikr_app/models.py
+++ hikr_app/models.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    description = models.TextField()
+    distance_hiked = IntegerField()
+    location = models.CharField(max_length=100)
+    photo_url = models.TextField()
+     
+    def __str__(self):
+      return self.author
