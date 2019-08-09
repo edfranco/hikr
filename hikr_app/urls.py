48394e91a693b4f750afb7889bfb9acb216b0e3f
Scottlove <haleyscottlove@gmail.com>
2019-08-09 11:04:34 -0700

    fixed post routes

--- hikr_app/urls.py
+++ hikr_app/urls.py
@@ -5,5 +5,5 @@ urlpatterns = [
     path('', views.home, name = 'home'),
     path('wall', views.wall, name = 'wall'),
     path('users/<int:pk>', views.profile, name= 'profile'),
-    path('new_hike', views.new_hike, name="new_hike")
+    path('new_hike/', views.new_hike, name="new_hike")
 ]
