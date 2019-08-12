1084522ff49b6ab3dae4ccb886e5b9631c13958d
edfranco <edfranco415@gmail.com>
2019-08-12 11:45:18 -0700

    created Like mode;l

--- hikr_app/admin.py
+++ hikr_app/admin.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-from .models import Post, Profile, Comment
+from .models import Post, Profile, Comment, Like
 from django.contrib import admin
 
 # Register your models here.
 admin.site.register(Post)
 admin.site.register(Profile)
 admin.site.register(Comment)
+admin.site.register(Like)
