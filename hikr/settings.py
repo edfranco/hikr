1dbe21dc23c3732a67012c29720154942452cdbc
edfranco <edfranco415@gmail.com>
2019-08-13 11:46:13 -0700

    heroku deployment

--- hikr/settings.py
+++ hikr/settings.py
@@ -121,8 +121,6 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
-STATIC_URL = '/static/'
-
 django_heroku.settings(locals())
 
 PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
